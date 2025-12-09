ca7c8512b3ce023d613f606d7185822513e0bb9b
jiang <geigleapplemandd4620@gmail.com>
2025-12-09 13:41:29 +0800

    添加查询用户名（email）接口

--- backend/Controllers/UsersController.cs
+++ backend/Controllers/UsersController.cs
@@ -57,5 +57,13 @@ namespace Controllers
                 return UsersService.CreateUser(admincreate);
             }
         }
+
+        // check username
+        [HttpPost("username")]
+        public HttpVO CheckUsername([FromQuery] string id)
+        {
+            HttpVO user = UsersService.CheckUsername(id);
+            return user;
+        }
     }
 }
