ee0d97d5752b9dae96229030a44ac1d0866a419f
jiang <geigleapplemandd4620@gmail.com>
2026-01-08 11:08:50 +0800

    更新S3

--- backend/Controllers/BookController.cs
+++ backend/Controllers/BookController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Service;
 
-namespace Controller
+namespace Controllers
 {
     [Route("book")]
     [ApiController]
---
+++ backend/Controllers/S3Controller.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace Controllers
+{
+    [ApiController]
+    [Route("file")]
+    public class S3Controller : ControllerBase
+    {
+
+        [HttpPost("s3")]
+        public async Task<HttpVO> UploadImage(IFormFile file)
+        {
+            HttpVO vo = new HttpVO();
+            
+            if (file == null || file.Length == 0)
+            {
+                vo.success = false;
+                vo.message = "No file uploaded.";
+                return vo;
+            }
+            try
+            {
+                // trun picture to base64
+                using var memoryStream = new MemoryStream();
+                await file.CopyToAsync(memoryStream);
+                byte[] fileBytes = memoryStream.ToArray();
+                string base64 = Convert.ToBase64String(fileBytes);
+                string lambdaUrl = "https://rtzd7j47xktqhcytnpihqiqaje0jxrtn.lambda-url.ap-southeast-2.on.aws/";
+
+                using var httpClient = new HttpClient();
+                var content = new StringContent(base64,Encoding.UTF8, "text/plain");
+                HttpResponseMessage response = await httpClient.PostAsync(lambdaUrl, content);
+
+                // AWS serverless response (lambda of S3)
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    vo = JsonSerializer.Deserialize<HttpVO>(responseBody)
+                  ?? new HttpVO { success = false, message = "Invalid response" };
+                }
+                else
+                {
+                    vo.success = false;
+                    vo.message = $"Lambda error: {response.StatusCode}";
+                }
+            }
+            catch (Exception ex)
+            {
+                vo.success = false;
+                vo.message = $"Upload failed: {ex.Message}";
+            }
+
+            return vo;
+        }
+    }
+}
